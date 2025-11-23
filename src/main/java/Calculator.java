e75110b878d094a1b691bfa1678f3916382bc84c
Maxim <144442618+surixes@users.noreply.github.com>
2025-11-23 22:45:16 +0300

    add func and tets, change README file (#2)
    Co-authored-by: Zimonin Maxim

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,33 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int dif(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+
+    public int div(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver() {
+        // Решение уравнения: 2x^2 + 3x - 5 для x=3
+        int x = 3;
+        int xSquared = times(x, x); // x² = 9
+        int term1 = times(2, xSquared); // 2x² = 18
+        int term2 = times(3, x); // 3x = 9
+        int positiveTerms = add(term1, term2); // 18 + 9 = 27
+        int result = dif(positiveTerms, 5); // 27 - 5 = 22
+
+        return result;
     }
 }
