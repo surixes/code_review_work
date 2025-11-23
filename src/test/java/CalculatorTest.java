b607addc60d33d26421a2ebc6001951e78b03c49
laandish <laandish@github.com>
2025-11-23 22:43:47 +0300

    fixed after code-review

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,31 +1,46 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class CalculatorTest {
 
-class CalculatorTest {
+    private Calculator calculator = new Calculator();
 
     @Test
-    void add() {
-        //TODO inser your realisation in test-method add
+    public void add() {
+        assertEquals(10, calculator.add(4, 6));
+        assertEquals(0, calculator.add(-5, 5));
+        assertEquals(-8, calculator.add(-3, -5));
     }
 
     @Test
-    void dif() {
-        //TODO inser your realisation in test-method dif
+    public void dif() {
+        assertEquals(2, calculator.dif(7, 5));
+        assertEquals(-2, calculator.dif(5, 7));
+        assertEquals(0, calculator.dif(10, 10));
     }
 
     @Test
-    void div() {
-        //TODO inser your realisation in test-method div
+    public void times() {
+        assertEquals(20, calculator.times(4, 5));
+        assertEquals(0, calculator.times(0, 5));
+        assertEquals(-15, calculator.times(3, -5));
     }
 
     @Test
-    void times() {
-        //TODO inser your realisation in test-method times
+    public void div() {
+        assertEquals(3, calculator.div(9, 3));
+        assertEquals(2, calculator.div(10, 5));
+        assertEquals(-4, calculator.div(12, -3));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivByZero() {
+        calculator.div(10, 0);
     }
 
     @Test
-    void solver() {
-        //TODO inser your realisation in test-method solver
+    public void solver() {
+        // (5 * 3) + (10 - 4) / 2 = 15 + 3 = 18
+        assertEquals(18, calculator.solver());
     }
 }
