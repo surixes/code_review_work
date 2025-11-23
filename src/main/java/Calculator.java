b6f45d329eac226b0d6e73bed1239c7bfa8738e2
laandish <laandish@github.com>
2025-11-23 22:15:04 +0300

    create calculator and tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,32 @@
 public class Calculator {
+
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
+
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
+
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
     }
+
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a * b;
     }
+
+    // (x² + 3x - 4) для x=2
     public int solver(){
-        //TODO inser your realisation in method solver
+        //  (5 * 3) + (10 - 4) / 2
+        int part1 = times(5, 3);      // 5 * 3 = 15
+        int part2 = dif(10, 4);       // 10 - 4 = 6
+        int part3 = div(part2, 2);    // 6 / 2 = 3
+        int result = add(part1, part3); // 15 + 3 = 18
+
+        return result;
     }
 }
