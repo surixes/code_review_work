8472f5dc4fa7a8100bfe8e4ba2411432c407f6bf
dfghjkl <dfghjkl>
2025-11-23 22:28:28 +0300

    add func and tets, change README file

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,30 +2,42 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CalculatorTest {
+public class CalculatorTest {
+
+    private Calculator calculator = new Calculator();
 
     @Test
-    void add() {
-        //TODO inser your realisation in test-method add
+    public void add() {
+        assertEquals(10, calculator.add(4, 6));
+        assertEquals(0, calculator.add(-5, 5));
+        assertEquals(-8, calculator.add(-3, -5));
     }
 
     @Test
-    void dif() {
-        //TODO inser your realisation in test-method dif
+    public void dif() {
+        assertEquals(2, calculator.dif(7, 5));
+        assertEquals(-2, calculator.dif(5, 7));
+        assertEquals(0, calculator.dif(10, 10));
     }
 
     @Test
-    void div() {
-        //TODO inser your realisation in test-method div
+    public void times() {
+        assertEquals(20, calculator.times(4, 5));
+        assertEquals(0, calculator.times(0, 5));
+        assertEquals(-15, calculator.times(3, -5));
     }
 
     @Test
-    void times() {
-        //TODO inser your realisation in test-method times
+    public void div() {
+        assertEquals(3, calculator.div(9, 3));
+        assertEquals(2, calculator.div(10, 5));
+        assertEquals(-4, calculator.div(12, -3));
     }
 
     @Test
-    void solver() {
-        //TODO inser your realisation in test-method solver
+    public void solver() {
+        // Проверяем: 2*(3)^2 + 3*3 - 5 = 2*9 + 9 - 5 = 18 + 9 - 5 = 22
+        assertEquals(22, calculator.solver2());
     }
+
 }
